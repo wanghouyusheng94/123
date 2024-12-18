27ac03b7ed5785eb6f78a3e6e35a3a8e5725d06e
DESKTOP-J3JN6OO\PC <zzzzz>
2024-12-18 11:48:04 +0800

    1

--- test.ts
+++ test.ts
@@ -1,17 +1,9 @@
 export const Config = {
 
-<<<<<<< HEAD
     server:"xxx",
     cdn:"xxx",
 
     // server:"xxxx",
     // cdn:"xxxx",
-=======
-    // server:"xxx",
-    // cdn:"xxx",
-
-    server:"xxxx",
-    cdn:"xxxx",
->>>>>>> test2
 
 }
